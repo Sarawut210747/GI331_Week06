6d9e4669c7cbe556ee442037c7ffc8fc49847751
ccalpiz <ccalpiz.sato@gmail.com>
2025-10-07 04:26:40 +0700

    Nice
    Not Nice

---
+++ Assets/Scripts/RankUIManager.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+
+public class RankUIManager : MonoBehaviour
+{
+    public GameObject rankDataPrefab;
+    public Transform rankPanal;
+
+    public List<PlayerData> playerDatas = new List<PlayerData>();
+    public List<GameObject> createdPlayerDatas = new List<GameObject>();
+
+    public RankData youRankData;
+    private void Start()
+    {
+        CreateRankData();
+    }
+    public void CreateRankData()
+    {
+        for (int i = 0; i < playerDatas.Count; i++)
+        {
+            GameObject rankObj = Instantiate(rankDataPrefab, rankPanal);
+            RankData rankData = rankObj.GetComponent<RankData>();
+
+            rankData.playerData = new PlayerData
+                (
+                playerDatas[i].rankNumber,
+                playerDatas[i].playerName,
+                playerDatas[i].playerScore,
+                playerDatas[i].profileImage
+
+                );
+
+            rankData.UpdateData();
+            createdPlayerDatas.Add(rankObj);
+        }
+    }
+
+    private void SortRankData()
+    {
+        List<PlayerData> sortRankPlayer = new List<PlayerData>();
+        sortRankPlayer = playerDatas.OrderByDescending(data => data.playerScore).ToList();
+
+        for (int i = 0; i < sortRankPlayer.Count; i++)
+        {
+            PlayerData changedRankNumber = sortRankPlayer[i];
+            changedRankNumber.rankNumber = i + 1;
+
+            sortRankPlayer[i] = changedRankNumber;
+        }
+        playerDatas = sortRankPlayer;
+    }
+    private void ClearRankData()
+    {
+        foreach (GameObject createdData in createdPlayerDatas)
+        {
+            Destroy(createdData);
+        }
+
+        createdPlayerDatas.Clear();
+    }
+
+    [ContextMenu("Reload")]
+    public void ReloadRankData()
+    {
+        ClearRankData();
+        SortRankData();
+        CreateRankData();
+    }
+}
